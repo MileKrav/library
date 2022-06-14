f7d1b0becdf2c950a5a99b97c4d2df7a1bd91867
ilyarudyi <ilya.rudyi@zaelab.com>
2022-06-14 18:07:48 +0300

    add books bage and sample

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+import styles from "../styles/About.module.css";
 
 const About = () => {
   return (
     <div>
       <Header />
-      About
+      <div className={styles.about__wrapper}>
+        <div> Тут может быть картинка </div>
+        <div>
+          {" "}
+          <div> А тут текст типо, что ресурс был создан того то</div>
+          того в результате выполнения дипломоной работы
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
--- pages/books.jsx
+++ pages/books.jsx
@@ -1,11 +1,44 @@
 import React from "react";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
+
+import styles from "../styles/Books.module.css";
 
 const Books = () => {
   return (
     <>
       <Header />
-      Тут будет вывод несколько книг
+      <div className={styles.books__wrapper}>
+        <div className={styles.book}>
+          <div className={styles.book__img}>
+            <p>Место для изображения</p>
+            <img />
+          </div>
+          <p>Краткое описание книги</p>
+        </div>
+        <div className={styles.book}>
+          <div className={styles.book__img}>
+            <p>Место для изображения</p>
+            <img />
+          </div>
+          <p>Краткое описание книги</p>
+        </div>
+        <div className={styles.book}>
+          <div className={styles.book__img}>
+            <p>Место для изображения</p>
+            <img />
+          </div>
+          <p>Краткое описание книги</p>
+        </div>
+        <div className={styles.book}>
+          <div className={styles.book__img}>
+            <p>Место для изображения</p>
+            <img />
+          </div>
+          <p>Краткое описание книги</p>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 };
